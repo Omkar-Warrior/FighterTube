f6524e0a9af936c4699097aa5b4681c6d37c9691
Omkar-Warrior <omkarjaykar23@gmail.com>
2026-03-31 12:25:42 +0530

    commit

---
+++ Frontend/JavaScript/Day1/externaljs.js
@@ -0,0 +1,9 @@
+
+
+var a=3;
+var b=4;
+console.log(a*b);            // It shows output on the Console
+document.writeln(a*b)        // It Shows Output on the webpage
+
+
+alert("Welcome to Scripting Language")
